e2c82488398bdc5689f435c03a03187233fa094f
tiejian147 <tiejian147@163.com>
2020-04-21 23:22:56 +0800

    学习while

---
+++ chapter5IfTest/ForTest07.java
@@ -0,0 +1,23 @@
+/*
+    编写for循环：找出1-100中所有的素数（质数）；
+*/
+
+
+public class ForTest07 {
+    public static void main(String[] args){
+
+        for(int i = 1; i <= 100; i++){
+
+            if(i %){
+
+            }
+        }
+
+    }
+}
+
+/*
+升级版：
+    编写for循环：找出1—10000中所有的素数；
+    要求每8个换一行输出；
+*/
