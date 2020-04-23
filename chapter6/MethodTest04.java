dc284016cc664748ddc357a98ef818d686bca5e4
刘生超 <shengchao.liu@yi23.net>
2020-04-23 18:20:50 +0800

    类名什么时候可以省略

---
+++ chapter6/MethodTest04.java
@@ -0,0 +1,18 @@
+
+
+public class MethodTest04{
+    public static void main(String[] args){
+        //调用daYin()方法
+        MethodTest04.daYin();
+        MethodTest04.daYin2();
+
+    }
+
+    public static void daYin(){
+        System.out.println("hello world");
+    }
+
+    public static void daYin2(){
+        System.out.println("hello world2");
+    }
+}
