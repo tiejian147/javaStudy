4a62d04e469c454a9d2ddc78c84c4b303f8847d3
tiejian147 <tiejian147@163.com>
2020-04-23 22:59:40 +0800

    不止是main方法可以调用其他方法

---
+++ chapter6/MethodTest05.java
@@ -0,0 +1,16 @@
+/*
+    
+ */
+
+public class MethodTest05 {
+    public static void main(){
+        //调用m1方法
+
+    }
+    public static void m1(){
+
+    }
+    public static void m2(){
+
+    }
+}
