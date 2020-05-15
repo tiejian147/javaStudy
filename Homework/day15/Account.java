8171d7e71a9684b0e57a03657b0a45513c14e94d
tiejian147 <tiejian147@163.com>
2020-05-16 00:35:18 +0800

    homeworkDay15

---
+++ Homework/day15/Account.java
@@ -0,0 +1,21 @@
+/**
+ * 二、写一个名为Account的类模拟账户
+ *      该类的属性和方法如下所示：
+ *      该类包括的属性：账户id，余额balance，年利率annualInterestRate：
+ *      包涵的方法：各属性的set和get方法，取款方法withdraw(),存款方法：deposit();
+ */
+
+public class Account {
+    //属性账户
+    String id;
+    //余额
+    float balance;
+    //getId的方法
+    public String getId(){
+        retrun id;
+    }
+    //setId的方法
+    public void setId(String UserAccountId){
+        balance = UserAccountId;
+    }
+}
