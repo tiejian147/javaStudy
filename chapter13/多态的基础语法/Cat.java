3ece6351b8cea62a1eb94f1ea1b7db01254e6b88
刘生超 <shengchao.liu@yi23.net>
2020-05-20 22:51:35 +0800

    多态3

--- chapter13/多态的基础语法/Cat.java
+++ chapter13/多态的基础语法/Cat.java
@@ -3,4 +3,8 @@ public class Cat extends Animal{
     public void move(){
         System.out.println("猫在走猫步~~~");
     }
+
+    public void catchMouse(){
+        System.out.println("猫会抓老鼠");
+    }
 }
